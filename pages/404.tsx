0fb4f6adb6ff2ecbd0079107746bc2d3f432b8b4
Gabriela Zuppardo <gabrielamzuppardo@gmail.com>
2023-05-11 20:04:56 -0300

    Add files via upload

---
+++ pages/404.tsx
@@ -0,0 +1,10 @@
+import { LinkNew } from "@/src/components/Link"
+
+export default function Page404(){
+    return(
+        <div>
+          <h1>Você se perdeu na net, essa página não existe</h1>
+          <LinkNew label="Ir para a Home Page" url="/" />
+      </div>
+    )
+}
